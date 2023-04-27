7d43574f91c59b5e19defd8dfc9705aab93f6982
Kshitij455 <88702135+Kshitij455@users.noreply.github.com>
2023-04-28 03:35:11 +0530

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,27 @@
+local objects = {} -- Table to store created object entities
+
+RegisterServerEvent('throwable:createObject')
+AddEventHandler('throwable:createObject', function(objectModel, x, y, z)
+    local modelHash = GetHashKey(objectModel)
+
+    RequestModel(modelHash)
+    while not HasModelLoaded(modelHash) do
+        Citizen.Wait(0)
+    end
+
+    local objectEntity = CreateObject(modelHash, x, y, z, true, false, true)
+    table.insert(objects, objectEntity)
+
+    TriggerClientEvent('throwable:objectCreated', -1, objectEntity)
+end)
+
+RegisterServerEvent('throwable:deleteObject')
+AddEventHandler('throwable:deleteObject', function(objectEntity)
+    for i, entity in ipairs(objects) do
+        if entity == objectEntity then
+            table.remove(objects, i)
+            break
+        end
+    end
+DeleteObject(objectEntity)
+end)
