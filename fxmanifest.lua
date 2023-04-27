50305a341a6f183831d1de6f3f418eebb8fa5c41
Kshitij455 <88702135+Kshitij455@users.noreply.github.com>
2023-04-28 03:29:42 +0530

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Kshitij Kalambe'
+description 'A script that allows players to pick up and throw objects.'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
