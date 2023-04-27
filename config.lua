442abc5b7c7dd3121b763fa792215ae5cdd8211b
Kshitij455 <88702135+Kshitij455@users.noreply.github.com>
2023-04-28 03:36:00 +0530

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+-- Object models that can be picked up
+Config.PickupObjects = {
+    'prop_cs_rub_binbag_01',
+    'prop_ld_jerrycan_01',
+    -- ...
+}
+
+-- Key bindings for pickup and throw
+Config.PickupKey = 'E'
+Config.ThrowKey = 'G'
+
+-- Throw force multiplier
+Config.ThrowForce = 100.0
