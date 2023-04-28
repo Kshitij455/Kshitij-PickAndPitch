ccafc8039234e10a68894324887fa72f5aa666ec
Kshitij455 <88702135+Kshitij455@users.noreply.github.com>
2023-04-28 14:00:04 +0530

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,87 +1,68 @@
+-- Load the necessary dependencies
+if Config.Framework == "qbcore" then
+    QBCore = exports['qb-core']:GetCoreObject()
+elseif Config.Framework == "esx" then
+    ESX = nil
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+end
+
 local pickedUpObject = nil
 
-RegisterNetEvent('throwable:objectCreated')
-AddEventHandler('throwable:objectCreated', function(objectEntity)
-    SetEntityAsMissionEntity(objectEntity, true, true)
-end)
-
-function PickupObject(object)
-    TaskStartScenarioInPlace(PlayerPedId(), "PROP_HUMAN_BUM_BIN", 0, 1)
-    Wait(1000)
-    DeleteEntity(object)
-    ClearPedTasksImmediately(PlayerPedId())
-    
-    -- Define the pickup animation
-    local animDict = "anim@heists@box_carry@"
-    local animName = "idle"
-    local flags = 49 -- Play the animation normally without repeating
-
-    -- Play the pickup animation
-    RequestAnimDict(animDict)
-    while not HasAnimDictLoaded(animDict) do
-        Citizen.Wait(0)
-    end
-    TaskPlayAnim(PlayerPedId(), animDict, animName, 8.0, -8.0, -1, flags, 0, false, false, false)
-    
-    TriggerServerEvent("Kshitij-PickAndPitch:GiveItem", object)
-end
-
-function AimAndShoot()
-    local target, distance = GetClosestPlayerInArea(2.0)
-    if target ~= nil and distance < 50.0 then -- Only allow shooting within 50 meters
-        local weapon = GetSelectedPedWeapon(PlayerPedId())
-        local damage = 30.0 -- Adjust the damage as needed
-        TriggerServerEvent("Kshitij-PickAndPitch:ApplyDamage", GetPlayerServerId(target), weapon, damage)
-    end
-end
-
-function GetClosestPlayerInArea(radius)
-    local playerPed = PlayerPedId()
-    local playerCoords = GetEntityCoords(playerPed)
-    local closestPlayer, closestDistance = nil, -1
-    for _, player in ipairs(GetActivePlayers()) do
-        local targetPed = GetPlayerPed(player)
-        if targetPed ~= playerPed then
-            local targetCoords = GetEntityCoords(targetPed)
-            local distance = Vdist(playerCoords, targetCoords)
-            if closestDistance == -1 or distance < closestDistance then
-                closestPlayer, closestDistance = player, distance
-            end
-        end
-    end
-    return closestPlayer, closestDistance
-end
-
+-- Handle picking up objects
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        if IsControlJustReleased(0, Keys[Config.ThrowKey]) then
-            ThrowObject()
-        elseif IsControlJustReleased(0, Keys[Config.PickupKey]) then
-            PickUpObject()
-        elseif IsControlPressed(0, 25) then -- Left mouse button
-            AimAndShoot()
+        
+        local playerPed = PlayerPedId()
+        local coords = GetEntityCoords(playerPed)
+        local targetObject = GetClosestObjectOfType(coords, Config.PickupDistance, GetHashKey(Config.PickupObject))
+
+        if IsControlJustPressed(0, Config.PickupControl) and not IsEntityDead(playerPed) and not IsPedInAnyVehicle(playerPed, true) then
+            if targetObject ~= 0 then
+                pickedUpObject = targetObject
+                
+                TaskPlayAnim(playerPed, Config.PickupAnimDict, Config.PickupAnimName, 8.0, -8.0, -1, 1, 0, false, false, false)
+
+                AttachEntityToEntity(pickedUpObject, playerPed, GetPedBoneIndex(playerPed, Config.PickupBoneIndex), Config.PickupOffset.x, Config.PickupOffset.y, Config.PickupOffset.z, Config.PickupRotation.x, Config.PickupRotation.y, Config.PickupRotation.z, true, false, false, true, 1, true)
+            end
+        end
+
+        -- Handle throwing objects
+        if pickedUpObject ~= nil and IsControlJustPressed(0, Config.ThrowControl) then
+            local forwardVector = GetEntityForwardVector(playerPed)
+
+            DetachEntity(pickedUpObject, true, false)
+
+            SetEntityCoords(pickedUpObject, coords.x + forwardVector.x, coords.y + forwardVector.y, coords.z + forwardVector.z, true, true, true, false)
+            ApplyForceToEntity(pickedUpObject, 1, forwardVector.x * Config.ThrowForceMultiplier, forwardVector.y * Config.ThrowForceMultiplier, forwardVector.z * Config.ThrowForceMultiplier, 0.0, 0.0, 0.0, true, true, true, true, true)
+            
+            pickedUpObject = nil
+
+            TaskPlayAnim(playerPed, Config.ThrowAnimDict, Config.ThrowAnimName, 8.0, -8.0, -1, 1, 0, false, false, false)
+
+            TriggerServerEvent("throwable:applyDamage")
         end
     end
 end)
 
-function ThrowObject()
-    if pickedUpObject ~= nil then
-        local playerPed = PlayerPedId()
-        local x, y, z = table.unpack(GetEntityCoords(playerPed))
-        local forwardX, forwardY = GetEntityForwardVector(playerPed)
-        local throwForce = 100.0
+-- Handle adding a throwable item to the player's inventory (for ESX framework)
+if Config.Framework == "esx" then
+    ESX.RegisterUsableItem(Config.ThrowableItem, function(source)
+        local xPlayer = ESX.GetPlayerFromId(source)
         
-        DetachEntity(pickedUpObject, true, false)
-        ApplyForceToEntity(pickedUpObject, 1, forwardX * throwForce, forwardY * throwForce, 1.0, 0.0, 0.0, 0.0, true, true, true, true, true, true)
-        pickedUpObject = nil
-    end
+        TriggerClientEvent("throwable:addObjectToList", source, Config.PickupObject)
+
+        xPlayer.removeInventoryItem(Config.ThrowableItem, 1)
+    end)
 end
 
-RegisterCommand('pickup', function()
-    PickUpObject()
-end)
+-- Handle adding a throwable item to the player's inventory (for QbCore framework)
+if Config.Framework == "qbcore" then
+    QBCore.Functions.CreateUseableItem(Config.ThrowableItem, function(source)
+        local Player = QBCore.Functions.GetPlayer(source)
+        
+        TriggerClientEvent("throwable:addObjectToList", source, Config.PickupObject)
 
-RegisterCommand('throw', function()
-    ThrowObject()
-end)
+        Player.Functions.RemoveItem(Config.ThrowableItem, 1)
+    end)
+end
